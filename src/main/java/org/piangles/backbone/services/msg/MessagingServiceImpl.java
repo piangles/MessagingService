5ab367caa794b055f8520ba03a9e8c625d8405fe
saradhivs <saradhivs@srinivasas-mbp>
2021-09-19 23:00:05 -0400

    Fixed partition number issue.

--- src/main/java/org/piangles/backbone/services/msg/MessagingServiceImpl.java
+++ src/main/java/org/piangles/backbone/services/msg/MessagingServiceImpl.java
@@ -91,7 +91,8 @@ public class MessagingServiceImpl implements MessagingService
 				topicConfig.put(TopicConfig.CLEANUP_POLICY_CONFIG, entityProperties.getCleanupPolicy());
 				topicConfig.put(TopicConfig.RETENTION_MS_CONFIG, String.valueOf(entityProperties.getRetentionPolicy()));
 
-				NewTopic newTopic = new NewTopic(topicName, entityProperties.getPartitionNo(), entityProperties.getReplicationFactor());
+				//ParitionNo=0 implies we need to create 1 Partition
+				NewTopic newTopic = new NewTopic(topicName, entityProperties.getPartitionNo()+1, entityProperties.getReplicationFactor());
 				newTopic.configs(topicConfig);
 
 				newTopics.add(newTopic);
